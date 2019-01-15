b0646425134ae9903d532bef5d005c1249003f4d
user148651 <user148651@77-240-12-25.rdns.melbourne.co.uk>
2019-01-15 10:59:26 +0000

    Solved index out of range bugs

--- Searcher/TableViewController.swift
+++ Searcher/TableViewController.swift
@@ -27,7 +27,16 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
         self.tableView.estimatedRowHeight = 600.0
         self.tableView.rowHeight = UITableView.automaticDimension
         
+        setUpSearchBar()
         
+        params["pos"] = String(self.nextOne)
+        
+        getGifData()
+    }
+    
+    //MARK: - Searchbar setup
+    
+    func setUpSearchBar() {
         let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width/1.5, height: 100.0))
         
         searchBar.searchBarStyle = .prominent
@@ -37,27 +46,50 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
         
         let leftNavBarButton = UIBarButtonItem(customView:searchBar)
         self.navigationItem.leftBarButtonItem = leftNavBarButton
-        
-        params["pos"] = String(self.nextOne)
-        
-        getGifData()
     }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.endEditing(true)
+    }
+    
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        let searchBar = self.navigationItem.leftBarButtonItem!.customView
+        if searchBar?.isHidden == true {
+            searchBar?.isHidden = false
+            searchBar?.endEditing(false)
+        }
+        else {
+            searchBar?.isHidden = true
+            searchBar?.endEditing(true)
+        }
+        
+    }
+    
+    //MARK: - Search bar delegate methods
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.nextOne = 0
+        params["q"] = searchText
+        params["pos"] = String(self.nextOne)
+        self.gifs.removeAll()
+        self.tableView.reloadData()
+        getGifData()
+        
+    }
+    
     
     //MARK: - Networking
     
     func getGifData() {
-        
-        
         Alamofire.request(GIF_URL, method: .get, parameters: params).responseJSON {
             response in
             if response.result.isSuccess {
                 print("Succes! Got the data.")
-                print(self.params)
+            
                 let gifJSON : JSON = JSON(response.result.value!)
-                
-                
                 self.processJSON(json: gifJSON)
-                print(self.gifs)
+                
                 self.tableView.reloadData()
                 
                 self.nextOne = self.nextOne + 5
@@ -65,7 +97,7 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
 
             }
             else {
-                print("Error! Could not get data.")
+               print("Error! Could not get data.")
             }
         }
     }
@@ -83,54 +115,21 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
             
         }
 }
-
-    @IBAction func searchButtonPressed(_ sender: Any) {
-        let searchBar = self.navigationItem.leftBarButtonItem!.customView
-        if searchBar?.isHidden == true {
-            searchBar?.isHidden = false
-            searchBar?.endEditing(false)
-        }
-        else {
-            searchBar?.isHidden = true
-            searchBar?.endEditing(true)
-        }
-        
-    }
     
     // MARK: - Table view data source
-    
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        self.tableView.reloadData()
-        tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
-        self.nextOne = 0
-        params["q"] = searchText
-        params["pos"] = String(self.nextOne)
-        self.gifs = []
-        self.tableView.reloadData()
-        getGifData()
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-        searchBar.endEditing(true)
-    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return gifs.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myTableViewCell", for: indexPath) as! TableViewCell
-    
-        
+
         let url = URL(string: gifs[indexPath.row])!
         let imageData = try? Data(contentsOf: url)
         
